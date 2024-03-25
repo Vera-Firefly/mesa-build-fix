61fe9a0d71222c7ed66a6b5ab2b4729b0b788e0d
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-03-25 17:22:15 +0800

    Update freedreno_device.c

--- src/freedreno/drm/freedreno_device.c
+++ src/freedreno/drm/freedreno_device.c
@@ -38,21 +38,26 @@ struct fd_device *msm_device_new(int fd, drmVersionPtr version);
 struct fd_device *virtio_device_new(int fd, drmVersionPtr version);
 #endif
 
+#ifdef HAVE_FREEDRENO_KGSL
+struct fd_device *kgsl_device_new(int fd);
+#endif
+
 struct fd_device *
 fd_device_new(int fd)
 {
    struct fd_device *dev = NULL;
-   drmVersionPtr version;
+   drmVersionPtr version = NULL;
    bool use_heap = false;
+   bool support_use_heap = true;
 
-   /* figure out if we are kgsl or msm drm driver: */
+/* figure out if we are kgsl or msm drm driver: */
+#ifdef HAVE_LIBDRM
    version = drmGetVersion(fd);
-   if (!version) {
+   if (!version)
       ERROR_MSG("cannot get version: %s", strerror(errno));
-      return NULL;
-   }
+#endif
 
-   if (!strcmp(version->name, "msm")) {
+   if (version && !strcmp(version->name, "msm")) {
       DEBUG_MSG("msm DRM device");
       if (version->version_major != 1) {
          ERROR_MSG("unsupported version: %u.%u.%u", version->version_major,
@@ -62,7 +67,7 @@ fd_device_new(int fd)
 
       dev = msm_device_new(fd, version);
 #ifdef HAVE_FREEDRENO_VIRTIO
-   } else if (!strcmp(version->name, "virtio_gpu")) {
+   } else if (version && !strcmp(version->name, "virtio_gpu")) {
       DEBUG_MSG("virtio_gpu DRM device");
       dev = virtio_device_new(fd, version);
       /* Only devices that support a hypervisor are a6xx+, so avoid the
@@ -70,10 +75,12 @@ fd_device_new(int fd)
        */
       use_heap = true;
 #endif
-#if HAVE_FREEDRENO_KGSL
-   } else if (!strcmp(version->name, "kgsl")) {
-      DEBUG_MSG("kgsl DRM device");
+#ifdef HAVE_FREEDRENO_KGSL
+   } else {
       dev = kgsl_device_new(fd);
+      support_use_heap = false;
+      if (dev)
+         goto out;
 #endif
    }
 
@@ -116,7 +123,7 @@ out:
       fd_pipe_del(pipe);
    }
 
-   if (use_heap) {
+   if (support_use_heap && use_heap) {
       dev->ring_heap = fd_bo_heap_new(dev, RING_FLAGS);
       dev->default_heap = fd_bo_heap_new(dev, 0);
    }
@@ -231,6 +238,12 @@ bool
 fd_dbg(void)
 {
    return debug_get_option_libgl();
+}
+
+uint32_t
+fd_get_features(struct fd_device *dev)
+{
+    return dev->features;
 }
 
 bool
