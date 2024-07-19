3c101b34cab338cfe74b872ebe1ab23fa08df71b
Vera-Firefly <1964762970@qq.com>
2024-07-19 13:17:30 +0800

    Zink & Freedreno Build Fix

--- src/gallium/auxiliary/target-helpers/inline_sw_helper.h
+++ src/gallium/auxiliary/target-helpers/inline_sw_helper.h
@@ -33,6 +33,18 @@
 #include "asahi/agx_public.h"
 #endif
 
+#ifdef GALLIUM_ZINK
+#include "zink/zink_public.h"
+#endif
+
+#ifdef GALLIUM_PANFROST
+#include "panfrost/pan_public.h"
+#endif
+
+#ifdef GALLIUM_FREEDRENO
+#include "freedreno/freedreno_public.h"
+#endif
+
 static inline struct pipe_screen *
 sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
 {
@@ -66,9 +78,32 @@ sw_screen_create_named(struct sw_winsys *winsys, const char *driver)
       screen = d3d12_create_dxcore_screen(winsys, NULL);
 #endif
 
+#if defined(GALLIUM_FREEDRENO)
+   if(screen == NULL && strcmp(driver, "freedreno") == 0) {
+      int kbase_device_fd = open("/dev/kgsl-3d0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+      if (kbase_device_fd == -1) { 
+         printf("FD_OSMESA: Failed to open kbase device: %s", strerror(errno));
+      } else {
+         struct pipe_screen_config dummy_cfg = { NULL, NULL };
+         screen = fd_screen_create(kbase_device_fd, &dummy_cfg, NULL);
+      }
+   }
+#endif
+
 #if defined(GALLIUM_ASAHI) && __APPLE__
    if (screen == NULL && strcmp(driver, "asahi") == 0)
       screen = agx_screen_create(0, NULL, winsys);
+#endif
+
+#if defined(GALLIUM_PANFROST)
+   if(screen == NULL && strcmp(driver, "panfrost") == 0) {
+      int kbase_device_fd = open("/dev/mali0", O_RDWR | O_CLOEXEC | O_NONBLOCK);
+      if(kbase_device_fd == -1) { 
+         printf("PAN_OSMESA: Failed to open kbase device: %s", strerror(errno));
+      }else {
+      	screen = panfrost_create_screen(kbase_device_fd, NULL);
+      }
+   }
 #endif
 
    return screen ? debug_screen_wrap(screen) : NULL;
