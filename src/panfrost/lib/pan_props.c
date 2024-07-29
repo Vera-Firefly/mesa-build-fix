b4a2357d435a598c23b232676ce459ca15e17359
WesleyVanNeck <wesleyvanneck1012@gmail.com>
2024-07-29 18:34:40 +0200

    fix unknown gpu

--- src/panfrost/lib/pan_props.c
+++ src/panfrost/lib/pan_props.c
@@ -80,6 +80,7 @@ const struct panfrost_model panfrost_model_list[] = {
 
 const struct panfrost_model panfrost_unknown_model = {
    .gpu_id = 0,
+   .gpu_variant = 0,
    .name = "Unknowm Mali device (Panfrost)",
    .performance_counters = "AAAA",
    .min_rev_anisotropic = NO_ANISO, 
