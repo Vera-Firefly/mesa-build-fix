7958b2b70a175980523b70200d5e02ebb4de18f7
Vera-Firefly <1964762970@qq.com>
2024-10-16 11:16:42 +0800

    修复x86_64的构建

--- src/mapi/glapi/glapi.h
+++ src/mapi/glapi/glapi.h
@@ -88,7 +88,7 @@ _GLAPI_EXPORT extern __THREAD_INITIAL_EXEC void * _glapi_tls_Context;
 _GLAPI_EXPORT extern const struct _glapi_table *_glapi_Dispatch;
 _GLAPI_EXPORT extern const void *_glapi_Context;
 
-#if DETECT_OS_WINDOWS && !defined(MAPI_MODE_UTIL) && !defined(MAPI_MODE_GLAPI)
+#if DETECT_OS_WINDOWS
 # define GET_DISPATCH() _glapi_get_dispatch()
 # define GET_CURRENT_CONTEXT(C)  struct gl_context *C = (struct gl_context *) _glapi_get_context()
 #else
